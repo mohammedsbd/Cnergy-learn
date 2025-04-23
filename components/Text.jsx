53f90816c503cbc87b93a7ca66784f3a31b4e631
Daniel Wagaye <danielwagaye1@gmail.com>
2025-04-23 22:58:54 +0300

    Update Text.jsx

--- components/Text.jsx
+++ components/Text.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Button } from './ui/button';
 import { toast } from 'sonner';
 
+// to create alert pop up
 const Text = () => {
 
     const handleClick = (mode) => {
