bab2a4cc7d7a4afc9a24caee57663ab9ccb85acc
mohammedsbd <mohammed1112ok@gmail.com>
2025-04-24 22:26:11 -0700

    populated courses with instructor

--- queries/courses.js
+++ queries/courses.js
@@ -1,12 +1,18 @@
 import {Course} from "@/model/course-model"
 import { Category } from "@/model/category-model";
+import { User } from "@/model/user-model";
 
 
 
 export async function getCourses() {
-  const courses = await Course.find({}).populate({
-    path: "category",
-    model: Category,
-  });
+  const courses = await Course.find({})
+    .populate({
+      path: "category",
+      model: Category,
+    })
+    .populate({
+      path: "instructor",
+      model: User,
+    });
     return courses
 }
