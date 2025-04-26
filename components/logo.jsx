dae58394322d7300fe7f7085be02c4928d522e7e
mohammedsbd <mohammed1112ok@gmail.com>
2025-04-26 08:18:04 -0700

    added main navigation header

--- components/logo.jsx
+++ components/logo.jsx
@@ -5,8 +5,15 @@ import { cn } from "@/lib/utils";
 
 const Logo = ({ className = "" }) => {
   return (
-    <div>
-      <Image className={cn("max-w-[80px]", className)} src={logo} alt="logo" />
+    <div className="flex min-h-screen flex-col">
+      <header className="z-40 bg-background/60 backdrop-blur-md fixed top-0 left-0 right-0 border-b">
+        <div className="container flex h-20 items-center justify-between py-6">
+          <MainNav items={navLinks} />
+        </div>
+      </header>
+
+      <main> {children} </main>
+      <SiteFooter />
     </div>
   );
 };
