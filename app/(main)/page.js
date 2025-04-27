5e169538204dd0585ec3854ebf5e2585e2856cca
mohammedsbd <mohammed1112ok@gmail.com>
2025-04-27 01:36:14 -0700

    finalized catgory

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -13,49 +13,49 @@ import Link from "next/link";
 import CourseCard from "./courses/_components/CourseCard";
 import { getCategories } from "@/queries/categories";
 
-// const categories = [
-//   {
-//     id: 1,
-//     title: "Design",
-//     thumbnail: "/assets/images/categories/design.jpg",
-//   },
+const categories = [
+  {
+    id: 1,
+    title: "Design",
+    thumbnail: "/assets/images/categories/design.jpg",
+  },
 
-//   {
-//     id: 3,
-//     title: "Development",
-//     thumbnail: "/assets/images/categories/development.jpg",
-//   },
-//   {
-//     id: 4,
-//     title: "Marketing",
-//     thumbnail: "/assets/images/categories/marketing.jpg",
-//   },
-//   {
-//     id: 5,
-//     title: "IT & Software",
-//     thumbnail: "/assets/images/categories/it_software.jpg",
-//   },
-//   {
-//     id: 6,
-//     title: "Personal Development",
-//     thumbnail: "/assets/images/categories/personal_development.jpg",
-//   },
-//   {
-//     id: 7,
-//     title: "Business",
-//     thumbnail: "/assets/images/categories/programming.jpg",
-//   },
-//   {
-//     id: 8,
-//     title: "Photography",
-//     thumbnail: "/assets/images/categories/photography.jpg",
-//   },
-//   {
-//     id: 9,
-//     title: "Music",
-//     thumbnail: "/assets/images/categories/music.jpg",
-//   },
-// ];
+  {
+    id: 3,
+    title: "Development",
+    thumbnail: "/assets/images/categories/development.jpg",
+  },
+  {
+    id: 4,
+    title: "Marketing",
+    thumbnail: "/assets/images/categories/marketing.jpg",
+  },
+  {
+    id: 5,
+    title: "IT & Software",
+    thumbnail: "/assets/images/categories/it_software.jpg",
+  },
+  {
+    id: 6,
+    title: "Personal Development",
+    thumbnail: "/assets/images/categories/personal_development.jpg",
+  },
+  {
+    id: 7,
+    title: "Business",
+    thumbnail: "/assets/images/categories/programming.jpg",
+  },
+  {
+    id: 8,
+    title: "Photography",
+    thumbnail: "/assets/images/categories/photography.jpg",
+  },
+  {
+    id: 9,
+    title: "Music",
+    thumbnail: "/assets/images/categories/music.jpg",
+  },
+];
 
 const HomePage = async () => {
   const courses = await getCourseList();
