95dbc4f68e8aa64ae6081c260bdd1ef275b38b5e
kidusm760 <kidusm760@gmail.com>
2025-06-26 17:31:09 +0300

    update routing

--- middleware.js
+++ middleware.js
@@ -1,10 +1,26 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import { NextResponse } from "next/server";
-import { PUBLIC_ROUTES, LOGIN, ROOT } from "@/lib/routes";
+import { PUBLIC_ROUTES, LOGIN, ROOT} from "@/lib/routes";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  // Code will be added here
+    const { nextUrl } = req;
+    const isAuthenticated = !!req.auth;
+
+    const isPublicRoute = PUBLIC_ROUTES.some((route) => nextUrl.pathname.startsWith(route)) || nextUrl.pathname === ROOT;
+
+    if (!isAuthenticated && !isPublicRoute) {
+        return NextResponse.redirect(new URL(LOGIN,nextUrl));
+    }
+    return NextResponse.next()
+
 });
+
+export const config = {
+    matcher: [
+      "/((?!api/auth|_next|favicon.ico|.*\\..*).*)", // Exclude Next.js internal routes and API routes
+      "/", // Include the root route
+    ],
+  };
