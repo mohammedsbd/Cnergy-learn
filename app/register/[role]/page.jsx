249d52f203d4cd2f548c47534f8032c31edbeaed
mohammedsbd <mohammed1112ok@gmail.com>
2025-06-30 03:16:14 -0700

    new fix

--- app/register/[role]/page.jsx
+++ app/register/[role]/page.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { SignupForm } from '../_components/signup-form';
 
@@ -11,20 +10,6 @@ const RegisterPage = ({ params: {role} }) => {
             
         </div>
     );
-=======
-"use server";
-import React from "react";
-import { SignupForm } from "../_components/signup-form";
-
-const RegisterPage = ({ params: { role } }) => {
-  return (
-    <div className="w-full flex-col h-screen flex items-center justify-center">
-      <div className="container">
-        <SignupForm role={role} />
-      </div>
-    </div>
-  );
->>>>>>> main
 };
 
 export default RegisterPage;
