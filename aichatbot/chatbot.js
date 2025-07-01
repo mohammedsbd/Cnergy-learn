12f009544de8c0d3ca73084dd4338b53be53f495
Abigiya <ajorken1502@gmail.com>
2025-07-01 16:21:51 +0300

    feat: add Botpress chatbot script

---
+++ aichatbot/chatbot.js
@@ -0,0 +1,9 @@
+window.botpressWebChat.init({
+  botId: "77907cf9-3686-4c7e-835a-e5cd6ccd4c27",
+  clientId: "77907cf9-3686-4c7e-835a-e5cd6ccd4c27",
+  hostUrl: "https://cdn.botpress.cloud/webchat/v1",
+  messagingUrl: "https://messaging.botpress.cloud",
+  lazySocket: true,
+  enableConversationDeletion: true,
+  showPoweredBy: false
+});
