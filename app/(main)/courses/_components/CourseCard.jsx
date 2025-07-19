e7e3184c52707a999c1764a756fdfa5bde501980
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-19 14:59:50 +1100

    add CourseCard component with course details and enroll button

--- app/(main)/courses/_components/CourseCard.jsx
+++ app/(main)/courses/_components/CourseCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import { Button, buttonVariants } from "@/components/ui/button";
@@ -6,56 +6,51 @@ import { ArrowRight } from "lucide-react";
 import { formatPrice } from "@/lib/formatPrice";
 import { ArrowRightIcon } from "lucide-react";
 import { BookOpen } from "lucide-react";
-
-const CourseCard = ({ course }) => {
-  return (
-    <Link key={course.id} href={`/courses/${course.id}`}>
-      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
-        <div className="relative w-full aspect-video rounded-md overflow-hidden">
-          <Image
-            src={`/assets/images/courses/${course?.thumbnail}`}
-            alt={"course"}
-            className="object-cover"
-            fill
-            unoptimized
-          />
-        </div>
-        <div className="flex flex-col pt-2">
-          <div className="text-lg md:text-base font-medium group-hover:text-sky-700 line-clamp-2">
+import EnrollCourse from '@/components/enroll-course';
+ 
+const CourseCard = ({course}) => {
+    return (
+        <Link key={course.id} href={`/courses/${course.id}`}>
+        <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+          <div className="relative w-full aspect-video rounded-md overflow-hidden">
+            <Image
+              src={`/assets/images/courses/${course?.thumbnail}`}
+              alt={"course"}
+              className="object-cover"
+              fill
+            />
+          </div>
+          <div className="flex flex-col pt-2">
+            <div className="text-lg md:text-base font-medium group-hover:text-sky-700 line-clamp-2">
             {course?.title}
-          </div>
-          <p className="text-xs text-muted-foreground">
-            {course?.category?.title}
-          </p>
-          <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
-            <div className="flex items-center gap-x-1 text-slate-500">
-              <div>
-                <BookOpen className="w-4" />
-              </div>
-              <span>{course?.modules?.length} Chapters</span>
             </div>
-          </div>
+            <p className="text-xs text-muted-foreground">{course?.category?.title}</p>
+            <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+              <div className="flex items-center gap-x-1 text-slate-500">
+                <div>
+                  <BookOpen className="w-4" />
+                </div>
+                <span>{course?.modules?.length} Chapters</span>
+              </div>
+            </div>
 
-          {/* <CourseProgress
+            {/* <CourseProgress
               size="sm"
               value={80}
               variant={110 === 100 ? "success" : ""}
             /> */}
 
-          <div className="flex items-center justify-between mt-4">
-            <p className="text-md md:text-sm font-medium text-slate-700">
-              {formatPrice(course?.price)}
-            </p>
+            <div className="flex items-center justify-between mt-4">
+              <p className="text-md md:text-sm font-medium text-slate-700">
+                {formatPrice(course?.price)}
+              </p>
 
-            <Button variant="ghost" className="text-xs text-sky-700 h-7 gap-1">
-              Enroll
-              <ArrowRight className="w-3" />
-            </Button>
+            <EnrollCourse asLink={true} />
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
-  );
+      </Link>
+    );
 };
 
 export default CourseCard;
