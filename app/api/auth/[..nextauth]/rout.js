030d54666a856d5ed81b3d0a75917d484b9e936c
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-29 12:14:20 +1100

    implement success page for Stripe checkout session
    Adds a new server component that handles displaying success messages after Stripe checkout completion.

--- app/api/auth/[..nextauth]/rout.js
+++ app/api/auth/[..nextauth]/rout.js
@@ -1,1 +1,16 @@
 export { GET, POST } from '@/auth';
+// app/api/auth/[...nextauth]/route.js
+
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
