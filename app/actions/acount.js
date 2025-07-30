ff91694090284b523958329ab15e4a4658b8bbd8
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-30 15:10:55 +1100

    add changePassword function with old password verification
    Created a secure password update handler that checks the old password before hashing and updating the new one in the database.

--- app/actions/acount.js
+++ app/actions/acount.js
@@ -5,3 +5,14 @@ import { validatePassword } from "@/queries/users";
 import { revalidatePath } from "next/cache"; 
 import bcrypt from 'bcryptjs';
 
+export async function updateUserInfo(email,updatedData){
+    try {
+        const filter = {email: email};
+        await User.findOneAndUpdate(filter,updatedData);
+        revalidatePath('/account');
+    } catch (error) {
+        throw new Error(error);
+    }
+
+}
+// End method 
