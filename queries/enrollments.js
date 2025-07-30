4af4030099c2713efcd2fd30e9df7955a2bd0a47
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-30 15:55:04 +1100

    implement enrollForCourse to create new enrollment records
    Added enrollForCourse function to enroll a user in a course with the selected payment method, defaulting status to not-started.

--- queries/enrollments.js
+++ queries/enrollments.js
@@ -1,10 +1,9 @@
-import {
-  replaceMongoIdInArray,
-  replaceMongoIdInObject,
-} from "@/lib/convertData";
-import { Enrollment } from "@/model/enrollment-model";
+import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData";
+import { Course } from "@/model/course-model";
+import { Enrollment } from "@/model/enrollment-model"; 
 
-export async function getEnrollmentsForCourse(courseId) {
-  const enrollments = await Enrollment.find({ course: courseId }).lean();
-  return replaceMongoIdInArray(enrollments);
+export async function getEnrollmentsForCourse(courseId){
+    const enrollments = await Enrollment.find({course: courseId}).lean();
+    return replaceMongoIdInArray(enrollments);
 }
+
