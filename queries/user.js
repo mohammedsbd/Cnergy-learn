75caef3e79047ca50e0940e20ba70dbe9a616900
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-30 15:34:25 +1100

    implement validatePassword to securely compare hashed passwords
    Added a validatePassword function that uses bcrypt.compare to check if the provided password matches the stored hash.

--- queries/user.js
+++ queries/user.js
@@ -2,3 +2,8 @@ import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData
 import { User } from "@/model/user-model";
 import bcrypt from "bcryptjs";
 
+export async function getUserByEmail(email){
+    const user = await User.findOne({email: email}).lean();
+    return replaceMongoIdInObject(user);
+} 
+
