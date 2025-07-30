19ee62754311cd14c7bd1e08c9ef79a358b0fd39
aaronkefyalew <aaronkefyalew@gmail.com>
2025-07-30 13:37:10 +1100

    redirect unauthenticated users to login page
    Added conditional logic to check if a user is authenticated. If not, users are redirected to /login to prevent unauthorized access to the sidebar.

--- app/(main)/account/component/account.sidebar.jsx
+++ app/(main)/account/component/account.sidebar.jsx
@@ -10,3 +10,27 @@ import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 import { getUserByEmail } from '@/queries/users';
 
+const AccountSidebar = async () => {
+
+    const session = await auth();
+    if (!session?.user) {
+        redirect("/login");
+    }
+
+    const loggedInUser = await getUserByEmail(session?.user?.email);
+    // console.log(loggedInUser);
+
+
+
+    return (
+<div className="lg:w-1/4 md:px-3">
+<div className="relative">
+    <div className="p-6 rounded-md shadow dark:shadow-gray-800 bg-white dark:bg-slate-900">
+        <div className="profile-pic text-center mb-5">
+            <input
+                id="pro-img"
+                name="profile-image"
+                type="file"
+                className="hidden"
+                    
+            />
